ab3db4be8cc30907b68c767c83918345d217fefc
Askhat Telzhanov <askhat491@gmail.com>
2018-11-03 18:43:22 +0600

    cardEvents

--- app/src/main/java/com/example/askhat/decathlon/events/EventsFragment.kt
+++ app/src/main/java/com/example/askhat/decathlon/events/EventsFragment.kt
@@ -1,2 +1,25 @@
 package com.example.askhat.decathlon.events
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.askhat.decathlon.R
+import kotlinx.android.synthetic.main.event_list.*
+import kotlinx.android.synthetic.main.event_list.view.*
+
+class EventsFragment: Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.event_list,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
